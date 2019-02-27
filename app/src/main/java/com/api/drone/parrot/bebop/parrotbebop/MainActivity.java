04b942fe704d6b1fcbe7983b5f8b3fca5c34e328
Benni Dietz <b_diet05@uni-muenster.de>
2019-02-27 20:05:45 +0100

    Initial commit

---
+++ app/src/main/java/com/api/drone/parrot/bebop/parrotbebop/MainActivity.java
@@ -0,0 +1,105 @@
+package com.api.drone.parrot.bebop.parrotbebop;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.parrot.arsdk.ARSDK;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryService;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // initialize discovery service for parrot drone
+        ARSDK.loadSDKLibs();
+        initDiscoveryService();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private ARDiscoveryService mArdiscoveryService;
+    private ServiceConnection mArdiscoveryServiceConnection;
+
+    private void initDiscoveryService()
+    {
+        // create the service connection
+        if (mArdiscoveryServiceConnection == null)
+        {
+            mArdiscoveryServiceConnection = new ServiceConnection()
+            {
+                @Override
+                public void onServiceConnected(ComponentName name, IBinder service)
+                {
+                    mArdiscoveryService = ((ARDiscoveryService.LocalBinder) service).getService();
+
+                    startDiscovery();
+                }
+
+                @Override
+                public void onServiceDisconnected(ComponentName name)
+                {
+                    mArdiscoveryService = null;
+                }
+            };
+        }
+
+        if (mArdiscoveryService == null)
+        {
+            // if the discovery service doesn't exists, bind to it
+            Intent i = new Intent(getApplicationContext(), ARDiscoveryService.class);
+            getApplicationContext().bindService(i, mArdiscoveryServiceConnection, Context.BIND_AUTO_CREATE);
+        }
+        else
+        {
+            // if the discovery service already exists, start discovery
+            startDiscovery();
+        }
+    }
+
+    private void startDiscovery()
+    {
+        if (mArdiscoveryService != null)
+        {
+            mArdiscoveryService.start();
+            Toast.makeText(this, "Start discovering...", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
